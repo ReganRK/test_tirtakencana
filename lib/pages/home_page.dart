4831c675ee6012c5be2c36ccd2cc89a6cc35da1d
ReganRK <reganreinal@gmail.com>
2023-10-18 15:24:02 +0700

    Homepage
    Add homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,54 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+class _HomePageState extends State<HomePage> {
+  List<String> items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+  String? _selectedItem = 'Item 1';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'this is home page'
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: DropdownButton(
+                    value: _selectedItem,
+                    items: items
+                      .map((item) => DropdownMenuItem<String>(
+                          value: item,
+                          child: Text(
+                            item
+                          )
+                        ),
+                      ).toList(), 
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedItem = value;
+                      });
+                    },
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    
+                  }, 
+                  child: Text(
+                    'Total Hadiah'
+                  )
+                ),
+              ],
+            )
+          ],
         ),
-      ),
+      )
     );
   }
 }
